9bca4ea9a7bcc80c5a40f07926e2e0c98aa1a4f9
SMOL <smolnero@gmail.com>
2025-05-29 19:55:03 -0600

    .cpp

--- core/memory/retained_heap.cpp
+++ core/memory/retained_heap.cpp
@@ -1,2 +1,12 @@
-#include "retained_heap.h"
+#include "retained_heap.h"kb
 #include <iostream>
+
+void RetainedHeap::retain(const std::string& id, cont std::string& content, bool core){
+	heap[id] = {id, content, core };
+	std::cout << "[Retained \"" << id << "\" => " << content << (core ? " (Core memory)"  : "") << std::endl;
+}
+
+
+void RetainedHeap::remove(const std::string& id) {
+	
+}
